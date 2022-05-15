870381e0eddf3e7c7fc2871aa94b759fe9eab443
991688344 <991688344@qq.com>
2022-05-15 13:41:19 +0800

    Site updated: 2022-05-15 13:41:16

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -1,128 +1,188 @@
-$(function() {
+window.addEventListener('load', () => {
+  let loadFlag = false
+  let dataObj = []
+  const $searchMask = document.getElementById('search-mask')
 
-    var loadFlag = false
-    // 预加载content.json
-    search(GLOBAL_CONFIG.localSearch.path)
-    loadFlag = true
-    $('a.social-icon.search').on('click', function() {
-        $('body').css('width', '100%')
-        $('body').css('overflow', 'hidden')
-        $('.search-dialog').animate({}, function() {
-            $('.search-dialog').css({
-                'display': 'block',
-                'animation': 'titlescale 0.5s'
-            }), 300
-        })
-        $('#local-search-input input').focus()
-
-        $('.search-mask').fadeIn();
-        if (!loadFlag) {
-            search(GLOBAL_CONFIG.localSearch.path)
-            loadFlag = true
-        }
-
-        // shortcut: ESC
-        document.addEventListener('keydown', function f(event) {
-            if (event.code === 'Escape') {
-                closeSearch()
-                document.removeEventListener('keydown', f)
-            }
-        })
+  const openSearch = () => {
+    const bodyStyle = document.body.style
+    bodyStyle.width = '100%'
+    bodyStyle.overflow = 'hidden'
+    btf.animateIn($searchMask, 'to_show 0.5s')
+    btf.animateIn(document.querySelector('#local-search .search-dialog'), 'titleScale 0.5s')
+    setTimeout(() => { document.querySelector('#local-search-input input').focus() }, 100)
+    if (!loadFlag) {
+      search()
+      loadFlag = true
+    }
+    // shortcut: ESC
+    document.addEventListener('keydown', function f (event) {
+      if (event.code === 'Escape') {
+        closeSearch()
+        document.removeEventListener('keydown', f)
+      }
     })
+  }
 
-    var closeSearch = function() {
-        $('body').css('overflow', 'auto')
-        $('.search-dialog').animate({}, function() {
-            $('.search-dialog').css({
-                'display': 'none'
+  const closeSearch = () => {
+    const bodyStyle = document.body.style
+    bodyStyle.width = ''
+    bodyStyle.overflow = ''
+    btf.animateOut(document.querySelector('#local-search .search-dialog'), 'search_close .5s')
+    btf.animateOut($searchMask, 'to_hide 0.5s')
+  }
+
+  const searchClickFn = () => {
+    document.querySelector('#search-button > .search').addEventListener('click', openSearch)
+  }
+
+  const searchClickFnOnce = () => {
+    document.querySelector('#local-search .search-close-button').addEventListener('click', closeSearch)
+    $searchMask.addEventListener('click', closeSearch)
+    if (GLOBAL_CONFIG.localSearch.preload) dataObj = fetchData(GLOBAL_CONFIG.localSearch.path)
+  }
+
+  // check url is json or not
+  const isJson = url => {
+    const reg = /\.json$/
+    return reg.test(url)
+  }
+
+  const fetchData = async (path) => {
+    let data = []
+    const response = await fetch(path)
+    if (isJson(path)) {
+      data = await response.json()
+    } else {
+      const res = await response.text()
+      const t = await new window.DOMParser().parseFromString(res, 'text/xml')
+      const a = await t
+      data = [...a.querySelectorAll('entry')].map(item =>{
+        return {
+          title: item.querySelector('title').textContent,
+          content: item.querySelector('content') && item.querySelector('content').textContent,
+          url: item.querySelector('url').textContent
+        }
+      })
+    }
+    if (response.ok) {
+      const $loadDataItem = document.getElementById('loading-database')
+      $loadDataItem.nextElementSibling.style.display = 'block'
+      $loadDataItem.remove()
+    }
+    return data
+  }
+
+  const search = () => {
+    if (!GLOBAL_CONFIG.localSearch.preload) {
+      dataObj = fetchData(GLOBAL_CONFIG.localSearch.path)
+    }
+
+    const $input = document.querySelector('#local-search-input input')
+    const $resultContent = document.getElementById('local-search-results')
+    const $loadingStatus = document.getElementById('loading-status')
+
+    $input.addEventListener('input', function () {
+      const keywords = this.value.trim().toLowerCase().split(/[\s]+/)
+      if (keywords[0] !== '') $loadingStatus.innerHTML = '<i class="fas fa-spinner fa-pulse"></i>'
+
+      $resultContent.innerHTML = ''
+      let str = '<div class="search-result-list">'
+      if (keywords.length <= 0) return
+      let count = 0
+      // perform local searching
+      dataObj.then(data => {
+        data.forEach(data => {
+          let isMatch = true
+          let dataTitle = data.title ? data.title.trim().toLowerCase() : ''
+          const dataContent = data.content ? data.content.trim().replace(/<[^>]+>/g, '').toLowerCase() : ''
+          const dataUrl = data.url.startsWith('/') ? data.url : GLOBAL_CONFIG.root + data.url
+          let indexTitle = -1
+          let indexContent = -1
+          let firstOccur = -1
+          // only match articles with not empty titles and contents
+          if (dataTitle !== '' || dataContent !== '') {
+            keywords.forEach((keyword, i) => {
+              indexTitle = dataTitle.indexOf(keyword)
+              indexContent = dataContent.indexOf(keyword)
+              if (indexTitle < 0 && indexContent < 0) {
+                isMatch = false
+              } else {
+                if (indexContent < 0) {
+                  indexContent = 0
+                }
+                if (i === 0) {
+                  firstOccur = indexContent
+                }
+              }
             })
-        })
-        $('.search-mask').fadeOut();
-    }
-    $('.search-mask, .search-close-button').on('click', closeSearch)
+          } else {
+            isMatch = false
+          }
 
-    function search(path) {
-        $.ajax({
-            url: GLOBAL_CONFIG.root + path,
-            dataType: 'json',
-            success: function(xmlResponses) {
-                // get the contents from search data
-                var datas = $(xmlResponses).map(function() {
-                    return {
-                        title: this.title,
-                        content: this.content,
-                        url: this.url
-                    }
-                }).get()
-                var $input = $('#local-search-input input')[0]
-                //---- 添加结果区域----
-                $('#local-stats').append('<div class="search-result-list" id="resultDis"></div>')
-                $input.addEventListener('input', function() {
-                    //var str = '<div class="search-result-list">'
-                    //------清除展示区域-----
-                    $('#resultDis').empty()
-                    //
-                    var keywords = this.value.trim().toLowerCase().split(/[\s]+/)
-                    //$resultContent.innerHTML = ''
-                    if (this.value.trim().length <= 0) {
-                        $('.local-search-stats__hr').hide()
-                        return
-                    }
-                    var count = 0
-                    // 去除content中的<xxx> 和 ![xxx](xxxx)图像内容
-                    var regSContent = new RegExp('!\\[[^\\]]*\\]\\([^\\)]*\\)|<[^>]+>|top_img: /.*(png|jpg|gif)|cover: /.*(png|jpg|gif)', "gi");
-                    // perform local searching
-                    datas.forEach(function(data) {
-                        var isMatch = true
-                        var dataTitle = data.title.trim().toLowerCase()
-                        var dataContent = data.content.trim().replace(regSContent, '').toLowerCase() //去除<xxx>
-                        var dataUrl = data.url
-                        var indexTitle = -1
-                        var indexContent = -1
-                        var tmpDisplayContent = ""
-                        // only match artiles with not empty titles and contents
-                        if (dataTitle !== '' || dataContent !== '') {
-                            keywords.forEach(function(keyword, i) {
-                                indexTitle = dataTitle.indexOf(keyword)
-                                indexContent = dataContent.indexOf(keyword)
-                                if (indexTitle < 0 && indexContent < 0) {
-                                    isMatch = false
-                                } else {
-                                    if (indexContent < 0) {
-                                        indexContent = 0
-                                    }
-                                }
-                            })
-                        }
-                        // show search results
-                        if (isMatch) {
-                            // 截取100长度的内容也展示出来,若只匹配了标题 则展示文章开头15个字符 ，对中间的关键词加粗处理
-                            tmpDisplayContent = dataContent.substring(indexContent - 50, indexContent + 50)
+          // show search results
+          if (isMatch) {
+            if (firstOccur >= 0) {
+              // cut out 130 characters
+              // let start = firstOccur - 30 < 0 ? 0 : firstOccur - 30
+              // let end = firstOccur + 50 > dataContent.length ? dataContent.length : firstOccur + 50
+              let start = firstOccur - 30
+              let end = firstOccur + 100
+              let pre = ''
+              let post = ''
 
-                            keywords.forEach(function(keyword) {
-                                var regS = new RegExp(keyword, "gi");
-                                tmpDisplayContent = tmpDisplayContent.replace(regS, '<mark style="color: white; background-color:green">' + keyword + '</mark>');
-                            });
+              if (start < 0) {
+                start = 0
+              }
 
-                            $('#resultDis').append(
-                                '<div style="border-top: 1px solid #dddddd" class="local-search__hit-item">' +
-                                '<a href="' + dataUrl + '" class="search-result-title">' +
-                                '<strong>《' + dataTitle + '》:</strong>   " ' + tmpDisplayContent + ' "' +
-                                '</a>' +
-                                '</div>'
-                            )
+              if (start === 0) {
+                end = 100
+              } else {
+                pre = '...'
+              }
 
-                            count += 1
-                            $('.local-search-stats__hr').show()
-                        }
-                    })
-                    if (count === 0) {
-                        str += '<div id="local-search__hits-empty">' + GLOBAL_CONFIG.localSearch.languages.hits_empty.replace(/\$\{query}/, this.value.trim()) +
-                            '</div>'
-                    }
-                    $('#local-hits').append("<p>未搜索到相关结果</p>")
-                })
+              if (end > dataContent.length) {
+                end = dataContent.length
+              } else {
+                post = '...'
+              }
+
+              let matchContent = dataContent.substring(start, end)
+
+              // highlight all keywords
+              keywords.forEach(keyword => {
+                const regS = new RegExp(keyword, 'gi')
+                matchContent = matchContent.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
+                dataTitle = dataTitle.replace(regS, '<span class="search-keyword">' + keyword + '</span>')
+              })
+
+              str += '<div class="local-search__hit-item"><a href="' + dataUrl + '" class="search-result-title">' + dataTitle + '</a>'
+              count += 1
+
+              if (dataContent !== '') {
+                str += '<p class="search-result">' + pre + matchContent + post + '</p>'
+              }
             }
+            str += '</div>'
+          }
         })
-    }
+        if (count === 0) {
+          str += '<div id="local-search__hits-empty">' + GLOBAL_CONFIG.localSearch.languages.hits_empty.replace(/\$\{query}/, this.value.trim()) +
+            '</div>'
+        }
+        str += '</div>'
+        $resultContent.innerHTML = str
+        if (keywords[0] !== '') $loadingStatus.innerHTML = ''
+        window.pjax && window.pjax.refresh($resultContent)
+      })
+    })
+  }
+
+  searchClickFn()
+  searchClickFnOnce()
+
+  // pjax
+  window.addEventListener('pjax:complete', () => {
+    !btf.isHidden($searchMask) && closeSearch()
+    searchClickFn()
+  })
 })
